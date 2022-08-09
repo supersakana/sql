57f8185ceb849ca51045cb667890aa804354bad3
supersakana <zacwilliamson@icloud.com>
2022-08-09 10:04:14 -0600

    finished exercise 3

--- 3_select_from_nobel.sql
+++ 3_select_from_nobel.sql
@@ -18,3 +18,42 @@ SELECT winner
 FROM nobel 
 WHERE subject = 'peace' AND yr > 1999;
 
+-- 5) Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive. 
+SELECT yr, subject, winner
+FROM nobel 
+WHERE subject = 'literature' 
+AND yr BETWEEN 1980 AND 1989;
+
+-- 6) Show all details of the presidential winners: 
+SELECT * 
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama'
+                  )
+
+-- 7) Show the winners with first name John 
+SELECT winner
+FROM nobel 
+WHERE winner LIKE 'John %';
+
+
+-- 8) Show the year, subject, and name of physics winners for 1980 together with the chemistry winners for 1984.
+SELECT yr, subject, winner
+FROM nobel
+WHERE (subject = 'Physics' AND yr = 1980) OR
+    (subject = 'Chemistry'AND yr = 1984);
+
+-- 9) Show the year, subject, and name of winners for 1980 excluding chemistry and medicine
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject NOT LIKE 'chemistry' AND
+      subject NOT LIKE 'medicine' AND
+      yr =1980;
+
+-- 10) Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT yr, subject, winner
+FROM nobel
+WHERE (yr < 1910 AND subject = 'medicine') OR
+(yr > 2003 AND subject = 'literature');
